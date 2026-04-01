5885d50fc88383a8bc1d26ee8621915b1304c704
Roma Loshak <sunnyboy7538@gmail.com>
2026-04-01 21:45:27 +0400

    init commit

---
+++ src/components/ChatWrapper.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  let value = $state('');
+</script>
+
+<input bind:value />
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Svelte chat app</title>
+</svelte:head>
+
+{@render children()}
